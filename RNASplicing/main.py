4b19366bc9ec788918fb7bf1f1b8c75e250d834d
radvect <pavel.buklemishev@gmail.com>
2023-04-02 23:46:13 +0300

    Added comparison function for the RNA Splicing problem

--- RNASplicing/main.py
+++ RNASplicing/main.py
@@ -1,4 +1,13 @@
 import FASTA_OPEN
 
+def comparison_sequances_for_nucleo(main, splice, index):
+    for i in splice:
+        if(i != main[index]):
+            return False
+        else:
+            index = index +1
+    return True
+
+
 if __name__ == '__main__':
     FASTA_OPEN.open_fasta_as_list("DNA.fasta")
