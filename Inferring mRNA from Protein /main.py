31a6ab35dcc19536dd0253ca541f26116c819b62
radvect <pavel.buklemishev@gmail.com>
2023-04-02 22:53:26 +0300

    -Solved Inferring mRNA from Protein (Combinatorics) problem

--- Inferring mRNA from Protein /main.py
+++ Inferring mRNA from Protein /main.py
@@ -1,9 +1,18 @@
-import Bio
+import Bio.Data.CodonTable
+d = Bio.Data.CodonTable.standard_rna_table.forward_table
 
-protein = str(input())
-
-length = len(protein)
+string = str(input())
+count = 0
+variations = 1;
+for i in string:
+    for item in d.values():
+        if(i==item):
+            count+=1
+    variations *= count
+    count = 0;
 
 number_of_stop_codons = 3
+variations = variations*number_of_stop_codons
 
+print(variations%1000000)
 
