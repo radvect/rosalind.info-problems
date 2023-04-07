211d87deeb15acfa934eea020bcfd6091857c1fe
radvect <pavel.buklemishev@gmail.com>
2023-04-07 22:16:24 +0300

    Continue solving RNA Splicing

--- RNASplicing/main.py
+++ RNASplicing/main.py
@@ -1,6 +1,8 @@
 import FASTA_OPEN
 
 def comparison_sequances_for_nucleo(main, splice, index):
+    if(len(splice)>len(main)-index):
+        return False
     for i in splice:
         if(i != main[index]):
             return False
@@ -10,4 +12,11 @@ def comparison_sequances_for_nucleo(main, splice, index):
 
 
 if __name__ == '__main__':
-    FASTA_OPEN.open_fasta_as_list("DNA.fasta")
+    fasta_list=FASTA_OPEN.open_fasta_as_list("DNA.fasta")
+    main_seq = fasta_list[0]
+    for i in range(len(main_seq)):
+        for j in range(1,len(fasta_list)):
+            if(comparison_sequances_for_nucleo(main_seq,fasta_list[j], i)):
+                main_seq = main_seq[:i]
+                i =0
+    print(main_seq)
