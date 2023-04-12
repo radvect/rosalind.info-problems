d214afb5331960d7bfdaef29f554c68809f128f9
radvect <pavel.buklemishev@gmail.com>
2023-04-13 01:31:23 +0300

    Drafts for Locating Restriction Sites problem

---
+++ LocatingRestrictionSites /main.py
@@ -0,0 +1,35 @@
+import sys
+
+
+import FASTA_OPEN
+sys.path.append( '/TranscribingDNAintoRNA' )
+for p in sys.path:
+    print( p )
+import main
+def LocationRestrictionSites(s):
+    a = ""
+    a = main.Transcription(s)
+    a = a[::-1]
+    i = 0
+    d = {}
+    while(i<len(s)):
+        l = 0
+
+        nexti = i+1
+        if(nexti>len(s)-1):
+            break
+        if(s[i] == a[i]):
+            if(s[nexti] == a[nexti]):
+                l = 2
+                indexofpal = i
+                while(s[i]!=a[i] and l<12 and i<len(s)):
+                    i = i+1
+                    l = l+1
+                d[indexofpal] = l
+
+        else:
+            i = i+1
+    print(d)
+
+a = FASTA_OPEN.open_fasta_as_list("LocatingRestrictionSites.fasta")
+LocationRestrictionSites(a[0])
