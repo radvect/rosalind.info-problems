681376479bfee7f86f64f1cd580a07d1063e28f6
radvect <pavel.buklemishev@gmail.com>
2023-04-15 01:19:51 +0300

    Solved the Locating Restriction Sites problem

--- LocatingRestrictionSites /main.py
+++ LocatingRestrictionSites /main.py
@@ -1,33 +1,32 @@
 import sys
-
+import copy
 
 import FASTA_OPEN
+import TranscribingDNAintoRNA.main
 from TranscribingDNAintoRNA import main
-def LocationRestrictionSites(s):
-    a = ""
+
+def reversePalindrom(s):
     a = main.Transcription(s)
     a = a[::-1]
+    if(s == a):
+        return True
+    else:
+        return False
+
+def LocationRestrictionSites(s):
+    print(s)
+
     i = 0
-    d = {}
     while(i<len(s)):
-        l = 0
+        lim = 12
+        if(len(s)-i<12):
+            lim = len(s)-i
+        for j in range(4,lim+1,2):
 
-        nexti = i+1
-        if(nexti>len(s)-1):
-            break
-        if(s[i] == a[i]):
-            if(s[nexti] == a[nexti]):
-                l = 2
-                indexofpal = i
-                while(s[i]!=a[i] and l<12 and i<len(s)):
-                    i = i+1
-                    l = l+1
-                d[indexofpal] = l
-
-        else:
-
-            i = i+1
-    print(d)
+            sub = s[i:i+j]
+            if(reversePalindrom(sub)):
+                print(str(i+1) + " " + str(j))
+        i = i+1
 
 a = FASTA_OPEN.open_fasta_as_list("LocatingRestrictionSites.fasta")
 LocationRestrictionSites(a[0])
